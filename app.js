70389a6b8a51cab525cb5473e26d44ef568ef202
Tony <tonynguyenit18@gmail.com>
2019-11-12 23:57:50 +1100

    add ssl

--- app.js
+++ app.js
@@ -1,25 +1,32 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const config = require("config");
+var fs = require('fs')
+var https = require('https')
 
 const api = require("./routers/api");
 
 const app = express();
 app.use(express.json());
-const server = require("http").Server(app);
+
+const port = process.env.PORT || 8080;
+// https.createServer({
+//     key: fs.readFileSync('server.key'),
+//     cert: fs.readFileSync('server.cert')
+// }, app).listen(port, () => console.log(`App listening on ${port}...`))
+
+const server = https.Server(app);
 const io = require("socket.io")(server);
 require("./routers/chat")(io);
 
 server.listen(3000, () => console.log("Socket listening on port 3000..."));
-
-const port = process.env.PORT || 8080;
-app.listen(port, () => console.log(`App listening on ${port}...`))
-
 
 const dbURI = config.get("mongoURI");
 mongoose.connect(dbURI).then(() => console.log("MongoDB is connected!"), {
     useNewUrlParser: true,
     dbName: "chatapp"
 }).catch(err => console.log(err))
+
+app.listen(port, () => console.log(`App listening on ${port}...`))
 
 app.use("/api", api);
